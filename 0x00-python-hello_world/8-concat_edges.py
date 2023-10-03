10fa6cad4fea674df4d1fda841e6770d4f94dd6e
adenijialiuadeyemi <adenijialiuadeyemi@gmail.com>
2023-10-02 19:05:09 -0700

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str_ = "Python is an interpreted, interactive, object-oriented programming \
-        language that combines remarkable power with very clear syntax"
-str_ = str_[39:67] + str_[107:112] + str_[:6]
-print(str_)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6] + "\n"
+print(str)
