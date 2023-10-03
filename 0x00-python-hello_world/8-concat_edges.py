298b35ca2229fc11fbd880ab5dec29a38ba61f6a
adenijialiuadeyemi <adenijialiuadeyemi@gmail.com>
2023-10-02 18:58:28 -0700

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+str_ = "Python is an interpreted, interactive, object-oriented programming \
         language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+str_ = str_[39:67] + str_[107:112] + str_[:6]
+print(str_)
