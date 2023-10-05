11e98429e2eaf267c5073b3df8b6475beebb4420
adenijialiuadeyemi <adenijialiuadeyemi@gmail.com>
2023-10-05 13:45:44 -0700

    calculator_1.py

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+def add(n1, n2):
+    """My addition function
+
+    Args:
+        n1: first integer
+        n2: second integer
+
+    Returns:
+        The return value. n1 + n2
+    """
+    return (n1 + n2)
+
+
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(num1, num2):
+    """My multiplication function
+
+    Args:
+        num1: first integer
+        num2: second integer
+
+    Returns:
+        The return value. num1 * num2
+    """
+    return (num1 * num2)
+
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
