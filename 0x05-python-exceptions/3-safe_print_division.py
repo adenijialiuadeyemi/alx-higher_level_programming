db611ae438cfea0e36fa49600ace71413a9558ff
adenijialiuadeyemi <adenijialiuadeyemi@gmail.com>
2023-10-23 12:57:57 -0700

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,8 @@
 def safe_print_division(a, b):
     try:
         div = a / b
-        return (div)
     except (ZeroDivisionError):
         div = None
     finally:
         print("result inside: {}".format(div))
+        return div
