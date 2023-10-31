fbf8124fe1495c05ea9be72c4ea862c9eb2c7bd3
adenijialiuadeyemi <adenijialiuadeyemi@gmail.com>
2023-10-30 21:39:29 -0700

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+File: 0-rectangle.py
+
+Author: Adeniji Aliu <adeijialiuadeyemi@gmail.com>
+
+Defines a class that represents a rectangle
+"""
+
+
+class Rectangle:
+    """
+    A class that defines a rectangle
+    """
+    pass
